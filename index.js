9e37d28a77d0acfbc999c2b0bede8c33efdf2daf
ashtkr01 <ashishkumarkatheriyaa@gmail.com>
2024-01-09 11:56:32 +0530

    Initialization of Project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//Socket:
+io.on("connection" , (socket) => {
+    socket.on("user-message" , (message) => {
+        console.log("A new user message :",message);
+        io.emit("message" , message);
+    });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/" , (req , res) => {
+    return res.sendFile("./public/index.html");
+});
+
+server.listen(9000 , () => {
+    console.log(`Server started at port 9000`);
+});
